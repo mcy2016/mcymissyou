c639779c7478c774036820494b8025d8fd4656a6
MCY <1991993249@qq.com>
2021-04-20 21:46:50 +0800

    init

---
+++ src/main/java/com/mcy/mcymissyou/McymissyouApplication.java
@@ -0,0 +1,13 @@
+package com.mcy.mcymissyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McymissyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McymissyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcy/mcymissyou/McymissyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcy.mcymissyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McymissyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
